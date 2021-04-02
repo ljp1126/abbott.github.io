556c7b73ed140d47a903b1d391cd64fa3e244ab7
Labbott <1006762873@qq.com>
2021-04-02 19:50:19 +0800

    测试一些

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./config/theme/')
 module.exports = {
   base: '/',
   title: 'Abbott',
-  description: '接天莲叶无穷碧，映日荷花别样红666',
+  description: '接天莲叶无穷碧，映日荷花别样红',
   dest: 'docs/.vuepress/dist',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
